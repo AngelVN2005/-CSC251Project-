2549fece108647919ec9dcf3d306a533b2ceb727
AngelVN2005 <AngelAntonioVillar@outlook.com>
2024-10-21 14:35:14 -0400

    policy class
    a class that will serve as a cookie cutter for the individual policies each customer has.

---
+++ Policy.java
@@ -0,0 +1,34 @@
+public class Policy{
+   
+   // policy class instance variables
+   String policyProvider;
+   String policyHolderName;
+   String policyHolderLastName;
+   int policyHolderAge;
+   boolean policyHolderSmoking;
+   double policyHolderHeight;
+   double policyHolderWeight;
+   int policyHolderBMI;
+   double policyPrice;
+   
+   //no arg constructor
+   private Policy(){
+   
+   }
+   
+   // policy constructor
+   public Policy(String provider; String name; String lName; int age; boolean smoking; double height; double weight; int bmi; double price){
+   
+   policyProvider = provider;
+   policyHolderName = name;
+   policyHolderLastName = lName;
+   policyHolderAge = age;
+   policyHolderSmoking = smoking; 
+   policyHolderHeight = height;
+   policyHolderWeight = weight;
+   policyHolderBMI = bmi;
+   policyPrice = price;
+   
+   }
+   
+}
--- Project_Angel_VillarNolasco.java
+++ Project_Angel_VillarNolasco.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
 public class Project_Angel_VillarNolasco {
    public static void main(String[]args){
    
-   System.out.println("Testing, testing, 1 2 3");
+   //defining the objects to be used later
+   Scanner scanObj = new Scanner(System.in); // new scanner object: "scanObj"
+   ArrayList<Policy> policyList = new ArrayList<Policy>(); // new array list of the policy class: "policylist"
+   
+   //main class variables to be used on the Policy class
+   String provider = "";
+   String name = ""; 
+   String lName = "";
+   int age = 0; 
+   boolean smoking = false; 
+   double height = 0.00; 
+   double weight = 0.00; 
+   int bmi = 0; 
+   double price = 0;
+   
+   //main class looping variables
+   boolean repeat;
+   
+   Policy customerPolicy = new Policy(provider, name, lName, age, smoking, height, weight, bmi, price);
+
    
    }
 }
