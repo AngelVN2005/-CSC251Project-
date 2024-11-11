9bae58f8098b0c0e144327b8e391730a3eb93136
AngelVN2005 <AngelAntonioVillar@outlook.com>
2024-11-11 04:10:53 -0500

    Transferred the appropriate variables and methods
    Transferred the appropriate variables and methods from the Policy class to the new  PolicyHolder class
    
    in addition i edited the policy class to be aggregated with the policyholder  and when it would usually need its own variables, it now calls the policyholder class to use its variables

--- Policy.java
+++ Policy.java
@@ -1,146 +1,84 @@
 public class Policy{
    
+   /**
+   STATIC CONSTANT FIELDS 
+   
+   these values will be used across all the policy objects created, without the need of setting new ones over and over
+   **/ 
+   private static final double CONVFACTOR = 703;
+   private static final double BASE_PRICE = 600;
+   private static final double ADDITIONAL_FEE_AGE = 75;
+   private static final double ADDITIONAL_FEE_SMOKING = 100;
+   private static final double ADDITIONAL_FEE_PER_BMI = 20;  
+   private static final int AGE_THRESHOLD = 50;
+   private static final int BMI_THRESHOLD = 35;
+   
    /** 
    INSTANCE VARIABLES
-   
-   the 'p' in front of the variable names stands for policy. 
-   I got tired of writting it over and over
    **/
    private String pProviderName;
    private String pNumber;
-   private String pHolderFirstName;
-   private String pHolderLastName;
-   private int pHolderAge;
-   private String pHolderSmokingStatus;
-   private double pHolderHeight;
-   private double pHolderWeight;
+   private PolicyHolder policyHolder;
    
    /** 
    NO-ARG CONSTRUCTOR
-   
-   now it actually initializes the variables
    **/
-   public Policy(){
-   
-   pProviderName = "";
-   pNumber = "";
-   pHolderFirstName = "";
-   pHolderLastName = "";
-   pHolderAge = 0;
-   pHolderSmokingStatus = "";
-   pHolderHeight = 0.00;
-   pHolderWeight = 0.00;
+   public Policy() { 
+      pProviderName = ""; 
+      pNumber = ""; 
+      policyHolder = new PolicyHolder();
    }
    
    /**
    POLICY CONSTRUCTOR
-   
-   imports values from the main method and set them to their respective counter parts
    **/
-   public Policy(String pPName, String number, String pHFName, String pHLName,int pHAge, String pHSStatus, double pHHeight, double pHWeight){
-      pProviderName = pPName;              //policy provider name
-      pNumber = number;                    //policy number
-      pHolderFirstName = pHFName;          //policy holder first name
-      pHolderLastName = pHLName;           //policy holder last name
-      pHolderAge = pHAge;                  //policy holder age
-      pHolderSmokingStatus = pHSStatus;    //policy holder smoking status
-      pHolderHeight = pHHeight;            //policy holder height
-      pHolderWeight = pHWeight;            //policy holder weight
+   public Policy(String pPName, String number, PolicyHolder holder) { 
+      pProviderName = pPName; 
+      pNumber = number; 
+      policyHolder = holder;
    }
    
    /**
-   INSTANCE METHODS
-   
-   get(something)
+   INSTANCE METHODS (GETTERS)
    **/
-   public String getPProviderName(){ //get policy provider name
-   return pProviderName;
-   }
-   public String getPNumber(){ //get policy number
-   return pNumber;
-   }
-   public String getPHolderFirstName(){ //get policy holder first name
-   return pHolderFirstName;
-   }
-   public String getPHolderLastName(){ //get policy holder last name
-   return pHolderLastName;
-   }
-   public int getPHolderAge(){ //get policy holder age
-   return pHolderAge;
-   }
-   public String getPHolderSmokingStatus(){ //get policy holder smoking status
-   return pHolderSmokingStatus;
-   }
-   public double getPHolderHeight(){ //get policy holder height
-   return pHolderHeight;
-   }
-   public double getPHolderWeight(){ //get policy holder weight
-   return pHolderWeight;
+   public String getPProviderName() { 
+      return pProviderName; 
+   } 
+   public String getPNumber() { 
+      return pNumber; 
+   } 
+   public PolicyHolder getPolicyHolder() { 
+      return policyHolder; 
    }  
    
    /**
-   INSTANCE METHODS
-   
-   set(something)
+   INSTANCE METHODS (SETTERS)
    **/
-   public void setPProviderName(String pPName){ //set policy provider name
-   pProviderName = pPName;
-   }
-   public void setPNumber(String number){ //set policy number
-   pNumber = number;
-   }
-   public void setPHolderFirstName(String pHFName){ //set policy holder first name
-   pHolderFirstName = pHFName;
-   }
-   public void setPHolderLastName(String pHLName){ //set policy holder last name
-   pHolderLastName = pHLName;
-   }
-   public void setPHolderAge(int pHAge){ //set policy holder age
-   pHolderAge = pHAge;
-   }
-   public void setPHolderSmokingStatus(String pHSStatus){ //set policy holder smoking status
-   pHolderSmokingStatus = pHSStatus;
-   }
-   public void setPHolderHeight(double pHHeight){ //set policy holder height
-   pHolderHeight = pHHeight;
-   }
-   public void setPHolderWeight(double pHWeight){ //set policy holder weight
-   pHolderWeight = pHWeight;
+   public void setPProviderName(String pPName) { 
+      pProviderName = pPName; 
+   } 
+   public void setPNumber(String number) { 
+      pNumber = number; 
+   } 
+   public void setPolicyHolder(PolicyHolder holder) { 
+      policyHolder = holder;
    }
    
    /**
    BMI CALCULATOR
    **/
-   public double getBMI()
-   {
-      final double CONVFACTOR = 703;
-      
-      return (pHolderWeight * CONVFACTOR) / (pHolderHeight * pHolderHeight);
+   public double getBMI() { 
+     return (policyHolder.getWeight() * CONVFACTOR) / (policyHolder.getHeight() * policyHolder.getHeight()); 
    }
+   
    /**
    POLICY PRICE CALCULATOR
    **/
-   public double getPrice()
-   {
-      final double BASE_PRICE = 600;
-      final double ADDITIONAL_FEE_AGE = 75;
-      final double ADDITIONAL_FEE_SMOKING = 100;
-      final double ADDITIONAL_FEE_PER_BMI = 20;
-      
-      final int AGE_THRESHOLD = 50;
-      final int BMI_THRESHOLD = 35;
-      
+   public double getPrice() { 
       double price = BASE_PRICE;
-      
-      if(pHolderAge > AGE_THRESHOLD)
-         price += ADDITIONAL_FEE_AGE;
-         
-      if(pHolderSmokingStatus.equalsIgnoreCase("smoker"))
-         price += ADDITIONAL_FEE_SMOKING;
-      
-      if(getBMI() > BMI_THRESHOLD)
-         price += ((getBMI() - BMI_THRESHOLD) * ADDITIONAL_FEE_PER_BMI);
-         
+         if (policyHolder.getAge() > AGE_THRESHOLD) price += ADDITIONAL_FEE_AGE; 
+         if (policyHolder.getSmokingStatus().equalsIgnoreCase("smoker")) price += ADDITIONAL_FEE_SMOKING; 
+         if (getBMI() > BMI_THRESHOLD) price += ((getBMI() - BMI_THRESHOLD) * ADDITIONAL_FEE_PER_BMI); 
       return price;
    }
 }
